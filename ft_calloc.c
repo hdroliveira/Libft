a70ea9ea5f728c44d0c478665e0ec33519d9fe3c
huda-roc <huda-roc@student.42porto.com>
2025-10-20 15:05:04 +0100

    Libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,14 +15,15 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-    if (!count || !size)
-    {
-        return (malloc(0));
-    }
-    if (count >= SIZE_MAX / size)
-    {
-        return (NULL);
-    }
+
+	if (!count || !size)
+	{
+		return (malloc(0));
+	}
+	if (count >= SIZE_MAX / size)
+	{
+		return (NULL);
+	}
 	ptr = malloc(count * size);
 	if (!ptr)
 	{
@@ -31,4 +32,3 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
-
